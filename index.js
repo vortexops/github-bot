c4176605eb88b65c9de7f2e06c5050a451c76d5b
vortexops <vortexops001@gmail.com>
2024-08-05 23:44:36 +0530

    add

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const simpleGit = require('simple-git');
 
 const FILE_PATH = './data.json';
 
-const makeCommit = (x, y) => {
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
+const makeCommit = (a, b, n) => {
+    if(n===0) return simpleGit().push();
+    const x = randomIntFromInterval(0, 54);
+    const y = randomIntFromInterval(0, 6);
     const DATE = moment().subtract(1, 'y').add(1, 'd')
                 .add(x, 'w').add(y, 'd').format();
     const data = {date: DATE}
+    console.log(DATE);
     jsonfile.writeFile(FILE_PATH, data, ()=>{
-        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
+        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}, makeCommit.bind(this, --n));
     });
 }
 
-makeCommit(3,3)
+makeCommit(1,2,500)
