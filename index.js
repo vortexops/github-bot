bfcdde306f86757f0a00c2001a669a1ec1887c27
vortexops <vortexops001@gmail.com>
2024-08-05 23:03:57 +0530

    add

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const simpleGit = require('simple-git');
 
 const FILE_PATH = './data.json';
 
-const DATE = moment().subtract(1, 'd').format();
+const DATE = moment().subtract(1, 'y').add(1, 'd').format();
 
 const data = {
     date: DATE
 }
 
-jsonfile.writeFile(FILE_PATH, data);
-
+jsonfile.writeFile(FILE_PATH, data, ()=>{
 //git commit --date="Mon"
-simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
+    simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
+});
+
