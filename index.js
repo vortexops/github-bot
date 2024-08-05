dbb89404a1d292288d7356ac70c21d3503c6ffa5
vortexops <vortexops001@gmail.com>
2024-08-06 00:07:12 +0530

    add

--- index.js
+++ index.js
@@ -8,31 +8,31 @@ function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-// const makeCommit = (a, b, n) => {
-//     if(n===0) {
-//         return simpleGit().push();
-//     }
-//     const x = randomIntFromInterval(0, 54);
-//     const y = randomIntFromInterval(0, 6);
-//     const DATE = moment().subtract(1, 'y').add(1, 'd')
-//                 .add(x, 'w').add(y, 'd').format();
-//     const data = {date: DATE}
-//     console.log(DATE);
-//     jsonfile.writeFile(FILE_PATH, data, ()=>{
-//         simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}, makeCommit.bind(this, --n));
-//     });
-// }
-
-// makeCommit(1, 2, 100)
-
-const startDate = moment().subtract(3, 'years');
-  const endDate = moment();
-
-  for (let date = startDate; date.isBefore(endDate); date.add(1, 'days')) {
-    const DATE = date.format();
-    const data = { date: DATE };
+const makeCommit = (n) => {
+    if(n===0) {
+        return simpleGit().push();
+    }
+    const x = randomIntFromInterval(0, 54);
+    const y = randomIntFromInterval(0, 6);
+    const DATE = moment().subtract(7, 'y').add(1, 'd')
+                .add(x, 'w').add(y, 'd').format();
+    const data = {date: DATE}
     console.log(DATE);
     jsonfile.writeFile(FILE_PATH, data, ()=>{
-        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
+        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}, makeCommit.bind(this, --n));
     });
-  }
+}
+
+makeCommit(100)
+
+// const startDate = moment().subtract(3, 'years');
+//   const endDate = moment();
+
+//   for (let date = startDate; date.isBefore(endDate); date.add(1, 'days')) {
+//     const DATE = date.format();
+//     const data = { date: DATE };
+//     console.log(DATE);
+//     jsonfile.writeFile(FILE_PATH, data, ()=>{
+//         simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
+//     });
+//   }
