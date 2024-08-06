c7466a9899da726ef9e0f46593b414617dcff9ee
vortexops <vortexops001@gmail.com>
2024-08-06 13:13:48 +0530

    update

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-const makeCommit = (n) => {
-    if(n===0) {
-        return simpleGit().push();
-    }
-    const x = randomIntFromInterval(0, 54);
-    const y = randomIntFromInterval(0, 6);
-    const DATE = moment().subtract(3, 'y').add(1, 'd')
-                .add(x, 'w').add(y, 'd').format();
-    const data = {date: DATE}
-    console.log(DATE);
-    jsonfile.writeFile(FILE_PATH, data, ()=>{
-        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}, makeCommit.bind(this, --n));
-    });
-}
+// const makeCommit = (n) => {
+//     if(n===0) {
+//         return simpleGit().push();
+//     }
+//     const x = randomIntFromInterval(0, 54);
+//     const y = randomIntFromInterval(0, 6);
+//     const DATE = moment().subtract(3, 'y').add(1, 'd')
+//                 .add(x, 'w').add(y, 'd').format();
+//     const data = {date: DATE}
+//     console.log(DATE);
+//     jsonfile.writeFile(FILE_PATH, data, ()=>{
+//         simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}, makeCommit.bind(this, --n));
+//     });
+// }
 
-makeCommit(500)
+// makeCommit(500)
 
 // const startDate = moment().subtract(3, 'years');
 //   const endDate = moment();
@@ -36,3 +36,15 @@ makeCommit(500)
 //         simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
 //     });
 //   }
+
+const startDate = moment().subtract(1, 'y');
+const endDate = moment();
+
+for (let date = startDate; date.isBefore(endDate); date.add(1, 'd')) {
+        const DATE = date.format();
+        const data = { date: DATE };
+        console.log(DATE);
+        jsonfile.writeFile(FILE_PATH, data, ()=>{
+            simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
+        });
+}
