d4eecffb0e4c7e96f4fa5e11b1aeaca81a60dd60
vortexops <vortexops001@gmail.com>
2024-08-06 13:22:37 +0530

    update

--- index.js
+++ index.js
@@ -8,43 +8,20 @@ function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-// const makeCommit = (n) => {
-//     if(n===0) {
-//         return simpleGit().push();
-//     }
-//     const x = randomIntFromInterval(0, 54);
-//     const y = randomIntFromInterval(0, 6);
-//     const DATE = moment().subtract(3, 'y').add(1, 'd')
-//                 .add(x, 'w').add(y, 'd').format();
-//     const data = {date: DATE}
-//     console.log(DATE);
-//     jsonfile.writeFile(FILE_PATH, data, ()=>{
-//         simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}, makeCommit.bind(this, --n));
-//     });
-// }
-
-// makeCommit(500)
-
-// const startDate = moment().subtract(3, 'years');
-//   const endDate = moment();
-
-//   for (let date = startDate; date.isBefore(endDate); date.add(1, 'days')) {
-//     const DATE = date.format();
-//     const data = { date: DATE };
-//     console.log(DATE);
-//     jsonfile.writeFile(FILE_PATH, data, ()=>{
-//         simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
-//     });
-//   }
-
-const startDate = moment().subtract(8, 'y');
-const endDate = moment();
-
-for (let date = startDate; date.isBefore(endDate); date.add(1, 'd')) {
-        const DATE = date.format();
-        const data = { date: DATE };
-        console.log(DATE);
-        jsonfile.writeFile(FILE_PATH, data, ()=>{
-            simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
-        });
+const makeCommit = (n) => {
+    if(n===0) {
+        return simpleGit().push();
+    }
+    const x = randomIntFromInterval(0, 54);
+    const y = randomIntFromInterval(0, 6);
+    const DATE = moment().subtract(7, 'y').add(1, 'd')
+                .add(x, 'w').add(y, 'd').format();
+    const data = {date: DATE}
+    console.log(DATE);
+    jsonfile.writeFile(FILE_PATH, data, ()=>{
+        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}, makeCommit.bind(this, --n));
+    });
 }
+
+makeCommit(100)
+
